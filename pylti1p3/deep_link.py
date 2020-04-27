f9f2e517808cb68834258ecca0f488fb72fc0ca4
dmitry-viskov <dmitry.viskov@webenterprise.ru>
2020-04-27 12:32:13 +0300

    Update mechanism of generating nonce (DeepLink)

--- pylti1p3/deep_link.py
+++ pylti1p3/deep_link.py
@@ -14,13 +14,16 @@ class DeepLink(object):
         self._deployment_id = deployment_id
         self._deep_link_settings = deep_link_settings
 
+    def _generate_nonce(self):
+        return uuid.uuid4().hex + uuid.uuid1().hex
+
     def get_message_jwt(self, resources):
         message_jwt = {
             'iss': self._registration.get_client_id(),
             'aud': [self._registration.get_issuer()],
             'exp': int(time.time()) + 600,
             'iat': int(time.time()),
-            'nonce': 'nonce-' + str(uuid.uuid4()),
+            'nonce': 'nonce-' + self._generate_nonce(),
             'https://purl.imsglobal.org/spec/lti/claim/deployment_id': self._deployment_id,
             'https://purl.imsglobal.org/spec/lti/claim/message_type': 'LtiDeepLinkingResponse',
             'https://purl.imsglobal.org/spec/lti/claim/version': '1.3.0',
@@ -30,7 +33,9 @@ class DeepLink(object):
         return message_jwt
 
     def encode_jwt(self, message):
-        encoded_jwt = jwt.encode(message, self._registration.get_tool_private_key(), algorithm='RS256')
+        headers = {'kid': self._registration.get_kid()}
+        encoded_jwt = jwt.encode(message, self._registration.get_tool_private_key(), algorithm='RS256',
+                                 headers=headers)
         return encoded_jwt.decode('utf-8') if sys.version_info[0] > 2 else encoded_jwt
 
     def get_response_jwt(self, resources):
