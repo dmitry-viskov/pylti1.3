68dccf04bf3c5a0a4950ed850aa4b3e8e6099dec
dmitry-viskov <dmitry.viskov@webenterprise.ru>
2020-05-02 00:04:36 +0300

    Ability to pass custom Request argument in DjangoOIDCLogin.__init__ / DjangoMessageLaunch.__init__

--- pylti1p3/contrib/django/oidc_login.py
+++ pylti1p3/contrib/django/oidc_login.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from pylti1p3.oidc_login import OIDCLogin
+from pylti1p3.request import Request
 from .cookie import DjangoCookieService
 from .redirect import DjangoRedirect
 from .request import DjangoRequest
@@ -9,7 +10,7 @@ from .session import DjangoSessionService
 class DjangoOIDCLogin(OIDCLogin):
 
     def __init__(self, request, tool_config, session_service=None, cookie_service=None, launch_data_storage=None):
-        django_request = DjangoRequest(request)
+        django_request = request if isinstance(request, Request) else DjangoRequest(request)
         cookie_service = cookie_service if cookie_service else DjangoCookieService(django_request)
         session_service = session_service if session_service else DjangoSessionService(request)
         super(DjangoOIDCLogin, self).__init__(django_request, tool_config, session_service, cookie_service,
