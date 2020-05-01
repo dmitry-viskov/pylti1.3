82d11d4fd9e054078d8e797969ce42c2a1f8d11b
dmitry-viskov <dmitry.viskov@webenterprise.ru>
2020-05-02 00:06:52 +0300

    Update DjangoRequest: ability to return default value in case if param not found

--- pylti1p3/contrib/django/request.py
+++ pylti1p3/contrib/django/request.py
@@ -4,18 +4,20 @@ from pylti1p3.request import Request
 class DjangoRequest(Request):
     _request = None
     _post_only = False
+    _default_params = None
 
-    def __init__(self, request, post_only=False):
+    def __init__(self, request, post_only=False, default_params=None):
         self.set_request(request)
         self._post_only = post_only
+        self._default_params = default_params if default_params else {}
 
     def set_request(self, request):
         self._request = request
 
     def get_param(self, key):
         if self._post_only:
-            return self._request.POST.get(key, None)
-        return self._request.GET.get(key, self._request.POST.get(key, None))
+            return self._request.POST.get(key, self._default_params.get(key))
+        return self._request.GET.get(key, self._request.POST.get(key, self._default_params.get(key)))
 
     def get_cookie(self, key):
         return self._request.COOKIES.get(key)
