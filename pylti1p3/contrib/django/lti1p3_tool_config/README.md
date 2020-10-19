b01b96b308afeb5230b671509bb56d1c8cd0720c
Clayton Turner <clayton@redshelf.com>
2020-10-19 10:42:14 -0500

    LMSI-343 Remove Django Models
    We are overwriting these files in our main repository and need
    to be able to extend them without having the migrations here
    cause headaches.

---
+++ pylti1p3/contrib/django/lti1p3_tool_config/README.md
@@ -0,0 +1,2 @@
+This directory has been cleared out and is implemented in another repository for easy extensibility
+See the following JIRA link for details: https://virdocs.atlassian.net/browse/LMSI-343
--- pylti1p3/contrib/django/lti1p3_tool_config/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig  # type: ignore
-
-
-class PyLTI1p3ToolConfig(AppConfig):
-    name = 'pylti1p3.contrib.django.lti1p3_tool_config'
-    verbose_name = "PyLTI 1.3 Tool Config"
